340f14ef2a3f5bc26dbd86a4a78548274728483d
SternGang <mkleeberg01@gmail.com>
2025-04-03 10:37:59 -0400

    another one

--- controllers/CadetRouter.js
+++ controllers/CadetRouter.js
@@ -33,7 +33,7 @@ cadetsRouter.post('/', (request, response, next) => {
             name: body.name,
             rank: body.rank,
             class: body.class,
-            email:body.userEmail,
+            userEmail:body.userEmail,
             Password: body.Password
         }
     )
